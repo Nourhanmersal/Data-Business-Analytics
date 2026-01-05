70522e84acf56d177b53fc37a7805cb625086b15
Nourhanmersal <nourhanmersal@hotmail.com>
2026-01-05 13:11:49 +0100

    Update Queries.sql

--- Zara Sales/Queries.sql
+++ Zara Sales/Queries.sql
@@ -2,7 +2,14 @@
 SELECT *
 FROM ZARA;
 
---Order count and sum of sales 
+--Number of Orders and Total Sales Value
 SELECT count ("Product ID") AS "Product count", 
 Sum ("Sales Volume") AS "sum of sales"
 FROM ZARA;
+
+--Product Position vs. Order Count and Sales
+SELECT "Product Position",
+count ("Product ID") AS "products count",
+Sum ("Sales Volume") AS "Sum of sales"
+FROM ZARA
+GROUP BY "Product Position";
